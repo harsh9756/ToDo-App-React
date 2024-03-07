9c8b9f547306a800105559af316985e851d40769
Harsh Upadhyay <harsh9756416665@gmail.com>
2024-03-07 13:26:47 +0530

    final

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className="flex justify-between bg-slate-900 text-white">
+            <div className="logo ml-9 my-3">
+                <span className="cursor-pointer text-xl hover:underline">React Todo App</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
